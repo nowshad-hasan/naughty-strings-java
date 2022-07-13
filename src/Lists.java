ee2ff371ea0257d50df788b48de4ccb299e0d827
Nowshad Hasan <nowshadapu@gmail.com>
2022-07-13 09:35:10 +0600

    Add Naughty Strings

---
+++ src/Lists.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Nowshad Hasan
+ * @since 13/7/22 9:05 am
+ */
+public class Lists {
+    private Lists() { } // can't be instantiated
+
+
+
+}
